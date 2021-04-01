3f9316b7abe64658c3a0cfc63612d0ff31788f22
Charlie Hoferer <charlie.hoferer@gmail.com>
2021-04-01 12:21:43 -0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace SyntaxSugarExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var answer = 4;
+            //var response = (answer < 9) ? answer + " is less than nine" : answer + " is greater than or equal to nine.";
+
+
+
+            var answer = 4;
+            string response;
+
+            if (answer < 9) 
+            {
+                response = ($"{answer} is less than nine");
+
+            }
+            else
+            {
+                response = ($"{answer} is greater than or equal to nine");
+            }
+
+
+            //if (answer < 9)
+            //{
+            //    response = answer + "is less than nine";
+
+            //}
+            //else
+            //{
+            //    response = answer + "is greater than or equal to nine";
+            //}
+            Console.WriteLine(response);
+
+           
+        }
+    }
+}
